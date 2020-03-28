6b94d7b921b2a9d82b3366f0903dc9c053ee7cfe
Krzem <50489938+Krzem5@users.noreply.github.com>
2020-03-28 18:21:35 +0100

    Push Update

---
+++ shop.py
@@ -0,0 +1,11 @@
+action=###1###
+1_,2_,3_=###2###,###3###,###4###
+d_=open('shop_d.txt','r')
+d=d.read()
+if action=='sell':d+='%s%s%s',%(1_,2_,3_)
+elif action=='buy':for line in d_:if line.startswith(1_):d.replace(line,'')
+d_.close()
+file=open('shop_d.txt','w')
+file.write(d)
+file.close()
+
